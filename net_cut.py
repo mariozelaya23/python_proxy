f0cc39dae6646565858a25c6f31e6789cc66ac52
Mario Zelaya <mariozelaya23@gmail.com>
2022-04-26 16:13:45 -0500

    adding netfilterqueue repository

--- net_cut.py
+++ net_cut.py
@@ -1,2 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import netfilterqueue
+
+
+def process_packet(packet):
+    print(packet)
+    # packet.drop()
+    packet.accept()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
+
 
